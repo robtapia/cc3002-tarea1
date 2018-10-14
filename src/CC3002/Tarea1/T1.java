89be1242b79da2261aaf94f74a03a29931929576
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2018-10-14 02:12:47 -0300

    Se crearon las clases necesarias

--- src/CC3002/Tarea1/T1.java
+++ src/CC3002/Tarea1/T1.java
@@ -1,4 +1,64 @@
 package CC3002.Tarea1;
 
 public class T1 {
+    interface attacker{
+        void attack(ente u);
+        int getAttackPoints();
+
+
+    }
+    interface attackable{
+        void atacked();
+        void beHealed();
+        int getHP();
+    }
+
+    public abstract class ente{
+
+    }
+
+    public abstract class Unit extends ente{
+        int initialHP;
+        int maxHP;
+        int attackPoints;
+        Boolean alive;
+        Boolean isAlive(){
+            return this.alive;
+        }
+    }
+
+    public class infantry extends Unit implements attacker, attackable {
+        public infantry(){
+            initialHP=100;
+            maxHP=2*initialHP;
+            attackPoints=25;
+            alive= true;
+        }
+    }
+    public class archer extends Unit {
+
+    }
+    public class cavalry extends Unit {
+
+    }
+    public class siege extends Unit {
+
+    }
+    public class monk extends Unit {
+
+    }
+    public class villager extends Unit {
+
+    }
+
+
+    public abstract class Building extends ente{
+
+    }
+    public class barracks extends Building{
+
+    }
+    public class castle extends Building{
+
+    }
 }
