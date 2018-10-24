782a8a8ea21bcab9778c2b346b821cd6d00ecafd
robtapia <roberto.tapia@ug.uchile.cl>
2018-10-24 19:18:01 +0100

    Se agrego la documentacion Javadoc para la clase ArcherUnit

--- src/CC3002/Tarea1/units/ArcherUnit.java
+++ src/CC3002/Tarea1/units/ArcherUnit.java
@@ -1,9 +1,20 @@
 package CC3002.Tarea1.units;
 
+/**@author Roberto Tapia
+ *
+ */
 public class ArcherUnit extends Unit {
+    /** Constructor de la clase ArcherUnit
+     *
+     */
     public ArcherUnit(){
         super(80,20);
     }
+
+    /** Ataca a un enemigo
+     *
+     * @param enemy enemigo que sera atacado
+     */
     public void attack(Attackable enemy) {
         if (this.isAlive()) {
             enemy.attackedByArcherUnit(this);
@@ -14,34 +25,63 @@ public class ArcherUnit extends Unit {
     }
 
 
-
+    /** Recibe un ataque de un objeto de la clase InfantryUnit
+     *
+     * @param enemy
+     */
     public void attackedByInfantryUnit(InfantryUnit enemy){
         this.doDamage(enemy,1.2);
     }
+
+    /** Recibe un ataque de un objeto de la clase ArcherUnit
+     *
+     * @param enemy
+     */
     public void attackedByArcherUnit(ArcherUnit enemy){
         this.doDamage(enemy,1.2);
     }
+
+    /** Recibe un ataque de un objeto de la clase CavalryUnit
+     *
+     * @param enemy
+     */
 
     public void attackedByCavalryUnit(CavalryUnit enemy) {
         this.doDamage(enemy,1.5);
 
     }
 
+    /** Recibe un ataque de un objeto de la clase SiegeUnit
+     *
+     * @param enemy
+     */
     public void attackedBySiegeUnit(SiegeUnit enemy) {
         this.doDamage(enemy,1.5);
 
     }
 
+    /** Recibe curacion de un monje
+     *
+     * @param ally
+     */
     public void attackedByMonk(Monk ally) {
         this.doDamage(ally,-.5);
 
     }
 
+    /** Recibe un ataque de un objeto de la clase Villager
+     *
+     * @param enemy
+     */
     public void attackedByVillager(Villager enemy) {
         this.doDamage(enemy,1);
 
     }
 
+    /** Recibe un ataque de un objeto de la clase Castle
+     *
+     * @param enemy
+     */
     public void attackedByCastle(Castle enemy) {
         this.doDamage(enemy,1.2);
 
